6fbccee8fa7b16429ca78ad3541ea2828d52a981
AMG_TECH_SEVVY <amdy2.gueye@ucad.edu.sn>
2024-09-04 13:59:36 +0200

    update

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const filmapp());
+}
+
+class filmapp extends StatelessWidget {
+  const filmapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+
+  }
+}
